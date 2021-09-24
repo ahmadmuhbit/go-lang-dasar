cef0552b28643d36ab63faf49ca735d8d81a4662
ahmadmuhbit <ahmadmuhbit@gmail.com>
2021-09-25 06:59:57 +0700

    Constant

---
+++ constant.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	//const firstName string = "Ahmad"
+	//const lastName = "Muhbit"
+	//const value = 1000
+
+	const (
+		firstName string = "Ahmad"
+		lastName         = "Muhbit"
+		value            = 1000
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+	fmt.Println(value)
+}
