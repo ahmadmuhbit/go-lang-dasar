b67473a1300c3098b88c0ae22f2000dd988220d8
ahmadmuhbit <ahmadmuhbit@gmail.com>
2021-09-24 15:08:06 +0700

    Tipe Data String

---
+++ string.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Ahmad")
+	fmt.Println("Muhbit")
+	fmt.Println("Go Lang")
+
+	fmt.Println(len("Ahmad"))
+	fmt.Println("Muhbit"[0])  // 77 (byte dari M)
+	fmt.Println("Go Lang"[1]) // 111 (byte dari o)
+}
