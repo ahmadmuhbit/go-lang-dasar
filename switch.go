1db51498df309450b2e0dab91c908a7a795bb780
ahmadmuhbit <ahmadmuhbit@gmail.com>
2021-09-28 10:14:19 +0700

    Switch Expression

---
+++ switch.go
@@ -0,0 +1,40 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	name := "Kurniawan"
+
+	switch name {
+	case "Muhbit":
+		fmt.Println("Hello Muhbit")
+		fmt.Println("Hello Muhbit")
+	case "Joko":
+		fmt.Println("Hello Joko")
+		fmt.Println("Hello Joko")
+	default:
+		fmt.Println("Kenalan dong")
+		fmt.Println("Kenalan dong")
+	}
+
+	// Switch dengan short statement
+	// switch length := len(name); length > 5 {
+	// case true:
+	// 	fmt.Println("Nama terlalu panjang")
+	// case false:
+	// 	fmt.Println("Nama sudah benar")
+	// }
+
+	// Switch tanpa kondisi
+	length := len(name)
+	switch {
+	case length > 10:
+		fmt.Println("Nama terlalu panjang")
+	case length > 5:
+		fmt.Println("Nama lumayan panjang")
+	default:
+		fmt.Println("Nama sudah benar")
+	}
+
+}
