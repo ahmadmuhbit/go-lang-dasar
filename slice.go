7f32e4ed41c6f9a497dbe7a9012fd1e4650ca178
ahmadmuhbit <ahmadmuhbit@gmail.com>
2021-09-28 08:28:15 +0700

    Tipe Data Slice

---
+++ slice.go
@@ -0,0 +1,64 @@
+package main
+
+import "fmt"
+
+func main() {
+	var months = [...]string{
+		"Januari",
+		"Februari",
+		"Maret",
+		"April",
+		"Mei",
+		"Juni",
+		"Juli",
+		"Agustus",
+		"September",
+		"Oktober",
+		"November",
+		"Desember",
+	}
+
+	var slice1 = months[4:7]
+	fmt.Println(slice1)
+	fmt.Println(len(slice1))
+	fmt.Println(cap(slice1))
+
+	// months[5] = "Diubah"
+	// fmt.Println(slice1)
+
+	// slice1[0] = "Mei Lagi"
+	// fmt.Println(months)
+
+	var slice2 = months[10:]
+	fmt.Println(slice2)
+
+	var slice3 = append(slice2, "Muhbit")
+	fmt.Println(slice3)
+	slice3[1] = "Bukan Desember"
+	fmt.Println(slice3)
+
+	fmt.Println(slice2)
+	fmt.Println(months)
+
+	// Kode program make slice
+	newSlice := make([]string, 2, 5)
+
+	newSlice[0] = "Ahmad"
+	newSlice[1] = "Muhbit"
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	// Kode program copy slice
+	copySlice := make([]string, len(newSlice), cap(newSlice))
+	copy(copySlice, newSlice)
+	fmt.Println(copySlice)
+
+	// Kode program array vs slice
+	iniArray := [5]int{1, 2, 3, 4, 5}
+	iniSlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(iniArray)
+	fmt.Println(iniSlice)
+}
